b306f1bb380ec117983b67a2a98be9b67990809e
sophialamala <153738349+sophialamala@users.noreply.github.com>
2024-11-29 10:34:25 +0100

    wip

--- b.php
+++ b.php
@@ -1,27 +1,48 @@
 <?php
-// etape 1: connexion a la base de donnée
+// Etape 1 -> Connexion a la base de données
 $server = "localhost";
 $username = "root";
-$password ="";
+$password = "";
 $databaseName = "accbbdd";
-
-// SELECT * FROM `user`
+ 
+// Connexion a la base de données
 $conn = new mysqli($server, $username, $password, $databaseName);
-
-//check connection
-if($conn->connect_error){
+ 
+// Check connection
+if($conn->connect_error) {
     die("connection BDD échouée");
 }
-// etape 2 -> execute requete sql
+ 
+// Etape 2 -> Execute requete SQL
 $sql = "SELECT * FROM `biomes`";
 $result = $conn->query($sql);
 $array = array();
-while($row = $result -> fetch_assoc()){
-    // on ajoute notre dico à la table des matières
-    $array[]=$row;
+// On parcours tous les resultats sql
+while($row = $result->fetch_assoc()) {
+    // On ajoute notre dictionnaire au tableau de resultats
+    $enclos_sql = "SELECT * FROM `enclos` WHERE id_biomes = '".$row["id"]."'";
+    $enclos_result = $conn->query($enclos_sql);
+ 
+    $enclos = array();
+    while($enclosRow = $enclos_result->fetch_assoc()) {
+ 
+        $animaux_sql = "SELECT * FROM `animaux` AS A INNER JOIN `relation` AS R ON A.id = R.id_animaux AND R.id_enclos = '".$enclosRow["id"]."'";
+        $animaux_result = $conn->query($animaux_sql);
+   
+        $animals = array();
+        while($animauxRow = $animaux_result->fetch_assoc()) {
+            $animaux[] = $animauxRow;
+        }
+        $enclosRow["animaux"] = $animaux;
+ 
+        $enclos[] = $enclosRow;
+    }
+ 
+    $row["enclos"] = $enclos;
+    $array[] = $row;
 }
-//var_dump($result); il retourne du html et ici on veut pas de html
 header('Content-Type:application/json');
 $json = json_encode($array);
 echo $json;
+ 
 ?>
